18f001c8205d543ee50c36a353d012b9f712585c
Sebastiaan Huber <sebastiaan.huber@epfl.ch>
2019-02-06 18:13:14 +0100

    Make `withmpi=False` the default for `get_default_options`
    All of calculations defined in this plugin are simple scripts that need
    to be run. They will most likely not benefit from running in MPI mode as
    they are relatively cheap computations, however, there is an added risk
    that the calculation will fail. Therefore it is better to have the
    default be to not run with MPI.

--- aiida_codtools/common/resources.py
+++ aiida_codtools/common/resources.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
 
-def get_default_options(num_machines=1, max_wallclock_seconds=1800):
+def get_default_options(num_machines=1, max_wallclock_seconds=1800, withmpi=False):
     """
     Return an instance of the options dictionary with the minimally required parameters
     for a JobCalculation and set to default values unless overriden
 
     :param num_machines: set the number of nodes, default=1
     :param max_wallclock_seconds: set the maximum number of wallclock seconds, default=1800
+    :param withmpi: if True the calculation will be run in MPI mode
     """
     return {
         'resources': {
             'num_machines': int(num_machines)
         },
         'max_wallclock_seconds': int(max_wallclock_seconds),
+        'withmpi': withmpi,
     }
